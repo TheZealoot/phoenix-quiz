c44ccdd29b129d1f17a5e63dbab30f54ec9cde39
Bojan Denda <boki.denda@gmail.com>
2023-11-04 12:11:53 +0100

    Added health check on root url

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import cors from "cors";
 import questionRouter from "./routes/question.routes";
 import recordRouter from "./routes/record.routes";
@@ -10,5 +10,11 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/api/v1/questions', questionRouter)
 app.use('/api/v1/records', recordRouter)
+
+app.get('/', (request: Request, response: Response) => {
+    response.status(200).send({
+        message: 'Phoenix Quiz is working!'
+    })
+})
 
 export default app

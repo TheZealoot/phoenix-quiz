3f982aeef9254effbe878aa4aaa1a85919ea0b0e
Bojan Denda <boki.denda@gmail.com>
2023-11-04 09:30:52 +0100

    Refactored for Cyclic

--- server.ts
+++ server.ts
@@ -6,14 +6,17 @@ dotenv.config()
 
 // @ts-ignore
 const Db = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD)
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8081
+
 mongoose
     .connect(Db)
     .then(() => {
         console.log('DB connection successful')
+        app.listen(PORT, () => {
+            console.log(`App listening on port ${PORT}`)
+        })
     })
-
-
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8081
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
+    .catch(err => {
+        console.error(err)
+        return false
+    })

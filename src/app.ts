d385100cc79ec17a6bec863c54008a031d69caec
Denda Bojan <bojan.denda@stagefronttickets.com>
2023-11-15 18:13:48 +0100

    Added CORS

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import recordRouter from "./routes/record.routes";
 
 const app = express()
 
-app.use(cors())
+app.use(cors({origin: 'https://phoenix-quizz.vercel.app/'}))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/api/v1/questions', questionRouter)
